babb2fff2256d2d8d50265196bd9bdde21e0cced
Samarth31# <deepikakohal27@gmail.com>
2024-10-12 14:59:06 +0400

    final

--- a.py
+++
@@ -1,7 +0,0 @@
-n=int(input("enter number: "))
-
-print(n)
-if n%2==0:
-    print("even")
-else:
-    print("odd")
--- b.py
+++ b.py
@@ -1,12 +1,12 @@
 n=int(input("enter number:"))
 
 print(n)
-if n >50:n
+if n >50:
     print("it is greater than 50")
     if n%2==0:
      print("even")
     else:
      
-     print("odd")
+     print("odd") 
 else:
-  print("number is less than 50")    
+ print("number is less than 50")
